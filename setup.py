e04a035ce50f3b60773239b12c48d36fbf74af3f
Yoendi <yoendigy1408@gmail.com>
2025-07-31 20:09:38 -0400

    adding setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name="task-tracker",
+    version="1.0.0",
+    description="A CLI application to manage tasks.",
+    author="Yoendi",
+    author_email="yoendigy1408@gmail.com",
+    # url="https://github.com/brkahmed/taskly",
+    py_modules=["task-tracker"],
+    entry_points={
+        "console_scripts": [
+            "task-tracker=task-tracker:main",
+        ],
+    },
+    tests_require=[
+        "pytest",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.12",
+)
